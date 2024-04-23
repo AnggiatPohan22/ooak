a24a72a04562dcada9e7350ea6c74139f1da75fe
AnggiatPohan22 <Anggiatcool1@gmail.com>
2024-04-23 13:23:59 +0700

    update header index

--- layout/header.php
+++ layout/header.php
@@ -2,7 +2,9 @@
     
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
+    <meta name="description" content="Find your perfect getaway at One Of A Kind Resort-Bintan Explore our collection of luxury & budget hotels & resorts worldwide. Book online & save with exclusive deals!">
+    <meta name="author" content="One Of A Kind Resort">
+    <title>One Of A Kind Resort Bintan - Nature & Uniq Concept Resort at Island Bintan</title>
     
     <!-- Favicons-->
     <link rel="shortcut icon" type="image/x-icon" sizes="16x16"  href="/img/logo-ooak.ico">
